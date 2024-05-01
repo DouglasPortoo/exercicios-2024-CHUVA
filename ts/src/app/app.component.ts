934d3d901c01bdbc59caeaa364f680a6b072ed26
Douglas <portogdouglas@gmail.com>
2024-05-01 18:21:22 -0300

    html base structure

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { NgComponentOutlet, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'DevChuva';
